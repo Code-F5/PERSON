946429b164029bec36c2209ce2c8d6b6e39ea8bd
Sumit Patil <patilsumit848@gmail.com>
2021-10-03 16:09:13 -0400

    Person profile project

---
+++ src/person/Person.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package person;
+
+/**
+ *
+ * @author Dell
+ */
+public class Person {
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+    
+    private String name;
+    private String address;
+    private String dob;
+    private String telephone;
+    private String fax;
+    private String email;
+    private String ssn;
+
+    
+    private String medicalrnumber;
+    private String healthplannumber;
+    private String bankaccount;
+    private String licencenumber;
+    private String linkedin;
+    private String ipaddress;
+    private String vehicle;
+    private String device;
+
+    
+    public String getBankaccount() {
+        return bankaccount;
+    }
+
+    public void setBankaccount(String bankaccount) {
+        this.bankaccount = bankaccount;
+    }
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+
+    public String getMedicalrnumber() {
+        return medicalrnumber;
+    }
+
+    public void setMedicalrnumber(String medicalrnumber) {
+        this.medicalrnumber = medicalrnumber;
+    }
+
+    public String getHealthplannumber() {
+        return healthplannumber;
+    }
+
+    public void setHealthplannumber(String healthplannumber) {
+        this.healthplannumber = healthplannumber;
+    }
+
+   
+
+    public String getLicencenumber() {
+        return licencenumber;
+    }
+
+    public void setLicencenumber(String licencenumber) {
+        this.licencenumber = licencenumber;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpaddress() {
+        return ipaddress;
+    }
+
+    public void setIpaddress(String ipaddress) {
+        this.ipaddress = ipaddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    
+
+}
